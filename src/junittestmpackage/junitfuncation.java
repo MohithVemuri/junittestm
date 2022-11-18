3889f2f27a641a0044fff07f4870d57c907d0014
anush <anush@10.12.14.229>
2022-11-18 22:53:40 +0530

    hello

---
+++ src/junittestmpackage/junitfuncation.java
@@ -0,0 +1,5 @@
+package junittestmpackage;
+
+public class junitfuncation {
+
+}
---
+++ src/junittestmpackage/testaddtwostrings.java
@@ -0,0 +1,27 @@
+package junittestm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testaddtwostrings{
+
+	@Test
+	public void test() {
+		junitfuncation obj = new junitfuncation();
+		int a = 10;
+		int b = 10;
+		assertEquals(20,obj.add_num(a, b));
+		
+	}
+	
+	@Test
+	public void test1() {
+		junitfuncation obj = new junitfuncation();
+		int a = 10;
+		int b = 10;
+		assertEquals(10,obj.add_num(a, b));
+		
+	}
+
+}
